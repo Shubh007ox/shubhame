9fb2241da22f7d0e1308f22f7b9b72dce91d46f4
Shubh007ox <110490602+Shubh007ox@users.noreply.github.com>
2022-08-04 05:17:24 -0700

    Update r.js

--- r.js
+++ r.js
@@ -1,1 +1,8 @@
-console.log('shusblng')
+let items = document.getElementsByClassName('list-group-item');
+console.log(items)
+console.log(items[1]);
+items[2].style.backgroundColor = 'green';
+
+for(i=0;i < items.length;i++) {
+    items[i].style.fontWeight = 'bold'
+}l
